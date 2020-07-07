0e3764078020a88d18c47ecfe2a818e19c3c28dd
Irvan Maulana <me@tukangremot.com>
2020-07-07 22:46:52 +0700

    fix: search, dont search to relationship method

--- src/traits/KTDatatable.php
+++ src/traits/KTDatatable.php
@@ -42,52 +42,13 @@ trait KTDatatable
 
     public static function scopeSearchDatatable($query, $keyword)
     {
-        $searchables = static::getSearchableColumns();
-        $searchOperator = static::getSearchOperator();
-        return $query->when($keyword, function ($query) use ($keyword, $searchables, $searchOperator) {
-            if ($searchables) {
-                $query->where(function ($query) use ($keyword, $searchables, $searchOperator) {
-                    function getGrouppedSearchable($field)
-                    {
-                        $splitted = explode('.', $field);
-                        if (count($splitted) > 1) {
-                            $key = array_shift($splitted);
-                            return [
-                                $key => count($splitted) > 1 ? getGrouppedSearchable(implode(
-                                    '.',
-                                    $splitted
-                                )) : [array_shift($splitted)]
-                            ];
-                        } else {
-                            return [array_shift($splitted)];
-                        }
-                    }
-                    
-                    $grouppedSearchables = [];
-                    foreach ($searchables as $field) {
-                        $grouppedSearchables = array_merge_recursive(
-                            $grouppedSearchables,
-                            getGrouppedSearchable($field)
-                        );
-                    }
-
-                    function buildGrouppedSearchablesQuery($query, $keyword, $relationship, $fields, $searchOperator)
-                    {
-                        if (is_array($fields)) {
-                            $query->orWhereHas($relationship, function ($query) use ($fields, $keyword, $searchOperator) {
-                                $query->where(function ($query) use ($keyword, $fields) {
-                                    foreach ($fields as $key => $field) {
-                                        buildGrouppedSearchablesQuery($query, $keyword, $key, $field, $searchOperator);
-                                    }
-                                });
-                            });
-                        } else {
-                            $query->orWhere($fields, $searchOperator, "%{$keyword}%");
-                        }
-                    }
-                    
-                    foreach ($grouppedSearchables as $relationship => $fields) {
-                        buildGrouppedSearchablesQuery($query, $keyword, $relationship, $fields, $searchOperator);
+        $fields = static::getSearchableColumns();
+        $operator = static::getSearchOperator();
+        return $query->when($keyword, function ($query) use ($keyword, $fields, $operator) {
+            if ($fields) {
+                $query->where(function ($query) use ($keyword, $fields, $operator) {
+                    foreach ($fields as $field) {
+                        $query->orWhere($field, $operator, "%{$keyword}%");
                     }
                 });
             }
